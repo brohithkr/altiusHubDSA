b1a97be56e1380026d6d1f67af38a8bbb5c6c1c6
brohithkr <brohithkr@gmail.com>
2025-06-05 12:21:38 +0530

    added question

---
+++ solution1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define pb push_back
+
+using namespace std;
+
+
+int helper(vector<int>& arr, int pos, int prev){
+    if(arr.size() == pos) return  0;
+    if(arr[pos] > prev){
+        return max(
+            1 + helper(arr, pos+1, arr[pos]),
+            helper(arr, pos+1, prev)
+        );
+    } else {
+        return helper(arr, pos+1, prev);
+    }
+}
+
+int main() {
+    vector<int> arr;
+    int num;
+    while(cin>>num){
+        arr.pb(num);
+        if(cin.peek() == '\n') break;
+    }
+    cout<<helper(arr, 0, -1);
+
+}
